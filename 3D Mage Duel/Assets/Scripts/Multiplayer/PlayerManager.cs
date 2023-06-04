755bb7fa53b4399d97aa56346138ecbbc589cc24
Nelly <luariya1212@gmail.com>
2023-06-04 12:33:02 +0200

    photon rigid body added

--- 3D Mage Duel/Assets/Scripts/Multiplayer/PlayerManager.cs
+++ 3D Mage Duel/Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -1,10 +1,13 @@
-using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
-    public GameObject playerPrefab1; // Reference to the first player prefab in the scene
-    public GameObject playerPrefab2; // Reference to the second player prefab in the scene
+    public GameObject playerPrefab; // Reference to the player prefab in the scene
 
     private void Start()
     {
@@ -13,24 +16,8 @@ public class PlayerManager : MonoBehaviourPunCallbacks
             // Check if this is the local player
             if (photonView.IsMine)
             {
-                // Find the appropriate player prefab based on player index
-                GameObject selectedPlayerPrefab;
-                if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
-                {
-                    selectedPlayerPrefab = playerPrefab1;
-                }
-                else if (PhotonNetwork.LocalPlayer.ActorNumber == 2)
-                {
-                    selectedPlayerPrefab = playerPrefab2;
-                }
-                else
-                {
-                    Debug.LogError("Invalid player index");
-                    return;
-                }
-
                 // Instantiate the player prefab and enable it
-                GameObject playerObj = PhotonNetwork.Instantiate(selectedPlayerPrefab.name, transform.position, transform.rotation);
+                GameObject playerObj = PhotonNetwork.Instantiate(playerPrefab.name, transform.position, transform.rotation);
                 playerObj.SetActive(true);
             }
         }
