67fadf9189fd4460d9e173d0ee34869a403d20a2
Nelly <luariya1212@gmail.com>
2023-06-04 12:38:48 +0200

    next try

--- 3D Mage Duel/Assets/Scripts/Movement/Jumping.cs
+++ 3D Mage Duel/Assets/Scripts/Movement/Jumping.cs
@@ -1,13 +1,7 @@
 using Photon.Pun;
-using Photon.Realtime;
-using Photon.Pun;
-using Photon.Realtime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Jumping : MonoBehaviourPunCallbacks
+public class Jumping : MonoBehaviourPunCallbacks, IPunObservable
 {
     [SerializeField] float Jumpforce = 5f;
     [SerializeField] float GroundDistance = 1f;
@@ -15,15 +9,13 @@ public class Jumping : MonoBehaviourPunCallbacks
     public LayerMask GroundMask;
 
     private bool isGrounded;
-
     private string deviceIdentifier;
-
-    private PhotonView photonView;
+    private Vector3 networkPosition;
+    private Quaternion networkRotation;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        photonView = GetComponent<PhotonView>();
     }
 
     private void Start()
@@ -32,18 +24,34 @@ public class Jumping : MonoBehaviourPunCallbacks
         {
             deviceIdentifier = PhotonNetwork.LocalPlayer.UserId;
         }
+        else
+        {
+            rb.isKinematic = true;
+        }
     }
 
     private void Update()
     {
         if (!photonView.IsMine)
         {
+            transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * 5);
             return;
         }
 
-        isGrounded = Physics.Raycast(transform.position, new Vector3(0, -1, 0), GroundDistance, GroundMask);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, GroundDistance, GroundMask);
 
         Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        // Update the network position and rotation
+        networkPosition = transform.position;
+        networkRotation = transform.rotation;
     }
 
     void Jump()
@@ -51,6 +59,22 @@ public class Jumping : MonoBehaviourPunCallbacks
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector3(rb.velocity.x, Jumpforce, rb.velocity.z), ForceMode.Impulse);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // Send the local player's position and rotation to the network
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // Receive the network player's position and rotation and update them
+            networkPosition = (Vector3)stream.ReceiveNext();
+            networkRotation = (Quaternion)stream.ReceiveNext();
         }
     }
 }
--- 3D Mage Duel/Assets/Scripts/Multiplayer/PlayerManager.cs
+++ 3D Mage Duel/Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -1,24 +1,18 @@
-using Photon.Pun;
-using Photon.Realtime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using Photon.Pun;
 
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
-    public GameObject playerPrefab; // Reference to the player prefab in the scene
+    public GameObject playerPrefab;
 
     private void Start()
     {
         if (PhotonNetwork.IsConnectedAndReady)
         {
-            // Check if this is the local player
             if (photonView.IsMine)
             {
-                // Instantiate the player prefab and enable it
                 GameObject playerObj = PhotonNetwork.Instantiate(playerPrefab.name, transform.position, transform.rotation);
-                playerObj.SetActive(true);
+                playerObj.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
             }
         }
     }
