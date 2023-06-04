30984d065c7c48f5fe32820c96d82bf207229b89
Nelly <luariya1212@gmail.com>
2023-06-04 11:50:30 +0200

    test number 7 keine ahnung

--- 3D Mage Duel/Assets/Scripts/Multiplayer/PlayerManager.cs
+++ 3D Mage Duel/Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -3,7 +3,8 @@ using Photon.Pun;
 
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
-    public GameObject playerPrefab; // Reference to the player prefab in the scene
+    public GameObject playerPrefab1; // Reference to the first player prefab in the scene
+    public GameObject playerPrefab2; // Reference to the second player prefab in the scene
 
     private void Start()
     {
@@ -12,8 +13,24 @@ public class PlayerManager : MonoBehaviourPunCallbacks
             // Check if this is the local player
             if (photonView.IsMine)
             {
+                // Find the appropriate player prefab based on player index
+                GameObject selectedPlayerPrefab;
+                if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+                {
+                    selectedPlayerPrefab = playerPrefab1;
+                }
+                else if (PhotonNetwork.LocalPlayer.ActorNumber == 2)
+                {
+                    selectedPlayerPrefab = playerPrefab2;
+                }
+                else
+                {
+                    Debug.LogError("Invalid player index");
+                    return;
+                }
+
                 // Instantiate the player prefab and enable it
-                GameObject playerObj = PhotonNetwork.Instantiate(playerPrefab.name, transform.position, transform.rotation);
+                GameObject playerObj = PhotonNetwork.Instantiate(selectedPlayerPrefab.name, transform.position, transform.rotation);
                 playerObj.SetActive(true);
             }
         }
